073a0e8a135c827ab4ee069d1cdd5133f7d0bb15
NizarAbiZaher <Nizabizaher@gmail.com>
2023-09-15 10:53:40 -0400

    fizz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,24 +1,21 @@
+
 const fizzBuzz = (userInput) => {
-    // Create counter using a for loop based on what the user inputs
-    for (let currentNumber = 0; currentNumber <= userInput; currentNumber++) {
-        // If the currentNumber is divisable by 3 print Fizz
-        if (currentNumber % 3 === 0) {
-            console.log("Fizz");
+    // Counter & Loop
+    for (let currentNumber = 1; currentNumber <= userInput; currentNumber++) {
+        // Check if the number is divisble by 3 & 5
+        if (currentNumber % 3 === 0 && currentNumber % 5 === 0) {
+            console.log('FizzBuzz');
         }
-        // If the currentNumber is divisable by 5 print Buzz
+        else if (currentNumber % 3 === 0) {
+            console.log('Fizz');
+        }
         else if (currentNumber % 5 === 0) {
-            console.log("Buzz");
+            console.log('Buzz');
         }
-        // If the currentNumber is divisable by 3 and 5 print FizzBuzz
-        else if (currentNumber % 3 === 0 && currentNumber % 5 === 0) {
-            console.log("FizzBuzz");
-        }
-        // Else print the currentNumber
         else {
             console.log(currentNumber);
         }
     }
 }
 
-// Example: 
-console.log(fizzBuzz(15))
+console.log(fizzBuzz(50))
